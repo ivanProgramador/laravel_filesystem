24f5b122613400b6e5c66867b23d282a27a8b3d5
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-16 18:39:37 -0300

    sobre  filesystem e clonagem de pasta

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::view('/','home');
+
+Route::get('/file',function(){
+   
+    $content = Storage::disk('public')->get('teste.txt');
+    echo $content;
+});
