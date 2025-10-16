1e21f2ed30ecfd688b988a034957f50a90c9b05b
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-10-16 11:27:20 -0300

    base do projeto filesystem pronta

---
+++ resources/views/components/main-layout.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
+    <title>Laravel Filesystem </title>
+</head>
+<body>
+     <div>
+        {{ $slot }}
+     </div>
+
+
+     <script src="{{ asset('assets/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
+
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<x-main-layout>
+     <div class="container mt-5">
+         <div class="row">
+             <div class="col">
+                 <p class="text-center display-3">Laboratório de Filesystem</p>
+                 <hr>
+                 
+
+             </div>
+         </div>
+     </div>
+</x-main-layout>
